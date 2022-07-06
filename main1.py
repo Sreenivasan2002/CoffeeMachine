8626b11da70aaa6c1fc281711464b15d104c2d00
Sreenivasan S <sreenivasanskpy@gmail.com>
2022-07-06 17:53:27 +0530

    Added Coffee Machine Project (python)

---
+++ main1.py
@@ -0,0 +1,106 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+profit=0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+def is_sufficient(order_incrediants):
+    print(order_incrediants)
+    for items in order_incrediants:
+        if order_incrediants[items] > resources["ingredients"][items]:
+            print("Sorry there is not enough water")
+            return False
+        return True
+
+
+def process_coins(drink, choice):
+    global profit
+    print("Incert coins:")
+    quarters=float(input("Quarters $"))
+    dimes = float(input("Dimes $"))
+    nickles = float(input("Nickles $"))
+    pennies = float(input("Pennies $"))
+    total = quarters*0.25 + dimes*0.10 + nickles*0.05 + pennies*0.01
+    print(round(f"Amount paid: ${total}"),2)
+    price = drink["cost"]
+    print(f"cost for the drink ${price}")
+    if total < price:
+        print("Sorry that's not enough money. Money refunded")
+        coffee_machine()
+
+    else:
+        balance =round(total - price,3)
+        print(f"Balance: ${balance}")
+        print(f"Here is your {choice}.Enjoy!")
+        profit =profit+ price
+
+
+def deduct_ingrediants(resources,drink):
+    for items in resources:
+        resources[items]=resources[items]-drink[items]
+
+
+
+
+def coffee_machine():
+    is_on = True
+    while is_on:
+
+        choice = input("What would you like? (espresso/latte/cappuccino):")
+        if choice == "off":
+            is_on = False
+        elif choice =="report":
+            drink = MENU[choice]
+            print(f"Water: {resources['water']}ml ")
+            print(f"Milk: {resources['milk']}ml ")
+            print(f"Coffee: {resources['coffee']}ml ")
+            print(f"Money: {profit}")
+
+            for items in resources:
+                resources[items] = resources[items] - drink["ingredients"][items]
+
+            # deduct_ingrediants(resources, drink)
+
+        else:
+            is_sufficient(drink["ingredients"])
+            process_coins(drink, choice)
+coffee_machine()
+
+
+
+
+
+
+
+
+
+
+
+
+
